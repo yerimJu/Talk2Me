04e83afe27acd01617a56fabd54385b70bf13100
Plurdis <uutak2000@naver.com>
2017-11-16 00:01:31 +0900

    LogWriter 파일 쓰기 구현

--- Analasing/LogWriter.py
+++ Analasing/LogWriter.py
@@ -1,19 +1,55 @@
-import os.path
+import datetime
 
 
 class LogWriter:
     def __init__(self, file_name):
         self.file_name = file_name
-        if not os.path.isfile(file_name):
-            raise FileNotFoundError()
-        else:
-            self.file = open(file_name, 'w')
+        self.file = open(file_name, 'a')  # if file not exists create new file
 
-    def write_log(self, message):
+    def write_log(self, message, with_date=True):
+        self.file = open(self.file_name, 'a')
+
+        if with_date:
+            message = '[' + self.get_date_string() + '] ' + message + '\n'
+
         self.file.write(message)
         self.file.close()
+
+    @staticmethod
+    def get_time(message):
+        # TODO: 메세지를 받아와서 그 메세지를 파싱해 시간 부분을 반환하기
+        pass
+
+    @staticmethod
+    def get_messagetype(message):
+        # TODO: 메세지 타입을 읽어와서 반환하기
+        pass
+
+    @staticmethod
+    def get_message(message):
+        # TODO: 시간을 제외한 메세지 부분만 반환하기
+        pass
+
+    def get_last_scrap_time(self):
+        self.file = open(self.file_name, 'r')
+
+        lines = self.file.readlines()
+
+        for line in list(reversed(lines)):
+            # TODO: reversed된 리스트를 분석해서 스크랩과 관련된 메세지가 나오게 되면 해당 부분의 시간을 반환하기
+            pass
+            
+        self.file.close()
+
+    @staticmethod
+    def get_date_string():
+        now = datetime.datetime.now()
+        return now.strftime('%Y-%m-%d %H:%M:%S')
 
 
 if __name__ == '__main__':
     lw = LogWriter('C:/Users/user/Desktop/test.txt')
-    lw.write_log('test')
+    for i in range(0, 10):
+        lw.write_log('test' + str(i))
+
+    lw.get_last_scrap_time()
