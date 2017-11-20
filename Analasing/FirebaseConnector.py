6dba9fad200aef7d465c0000434107e2fdc4ad3f
Plurdis <uutak2000@naver.com>
2017-11-20 21:27:01 +0900

    FirebaseConnector 사용자 정보 가져오기

--- Analasing/FirebaseConnector.py
+++ Analasing/FirebaseConnector.py
@@ -9,9 +9,23 @@ def load():
                                             'databaseURL' : 'https://talk2me-58045.firebaseio.com/'
                                         })
 
-    ref = db.reference('/notifications')
+    ref = db.reference('/users')
+    users_dict = ref.get()
 
-    print(ref.get())
+    ref = db.reference('/notifications')
+    notification_dict = ref.get()
+
+    for user in users_dict:
+        print('user token : ', user)
+        # print(users_dict[user])
+        try:
+            print('facebook Access Token : ', users_dict[user]['facebookAccesstoken'])
+        except KeyError:
+            print("this User dosen't have FaceBook Access Token")
+        print('email : ', users_dict[user]['email'])
+        print('username : ', users_dict[user]['username'])
+        print()
+
 
 if __name__ == '__main__':
     load()
