255951016b39e2378264c99b99f6f38918f1fafa
Plurdis <uutak2000@naver.com>
2017-12-11 00:34:31 +0900

    스톱워치 구현, RSS 수집기 구성 변경

--- Analasing/RSSCollector.py
+++ Analasing/RSSCollector.py
@@ -1,22 +1,39 @@
 import feedparser
 import BaseCollector
+import datetime
+import json
 
 
 class RSSCollector(BaseCollector.BaseCollector):
     def __init__(self):
         super(RSSCollector, self).__init__()
+        self.last_time = {}
 
-    @staticmethod
-    def print_test(url):
+        # 내부적으로는 json으로 저장
+        self.url_list = {}
+
+    def update_item(self, url):
         feed = feedparser.parse(url)
 
+        # TODO: 오류 수정
+        url_dict = {'feeds', dict()}
+
         for key in feed['entries']:
-            title = key['title']
-            publish_date = key.published
-            description = key['description']
-            category = key['category']
-            link = key['link']
-            print(title, publish_date, category)
+            feed_data = {'title': key['title'],
+                         'publish': key.published,
+                         'description': key['description'],
+                         'category': key['category'],
+                         'link': key['link']}
+
+            print(feed_data)
+
+            url_dict['feeds'].append(feed_data)
+
+        self.last_time[self.url_normalization(url)] = datetime.datetime.now()
+
+    @staticmethod
+    def url_normalization(url):
+        return url.lower()
 
     def get_csv(self):
         pass
@@ -27,4 +44,4 @@ class RSSCollector(BaseCollector.BaseCollector):
 
 if __name__ == '__main__':
     rc = RSSCollector()
-    rc.print_test('http://blog.rss.naver.com/uutak2000.xml')
+    rc.update_item('http://blog.rss.naver.com/uutak2000.xml')
--- Analasing/Stopwatch.py
+++ Analasing/Stopwatch.py
@@ -1,13 +1,28 @@
 import datetime
 
+
 class Stopwatch:
     def __init__(self):
+        self.running = False
         self.start_time = datetime.datetime.now()
-        self.end_time = datetime.datetime.now()
+        self.end_time = self.start_time
         pass
 
     def start(self):
         self.start_time = datetime.datetime.now()
+        self.running = True
+
+    def stop(self):
+        self.end_time = datetime.datetime.now()
+        self.running = False
+
+    def reset(self):
+        self.start_time = datetime.datetime.now()
+        self.end_time = self.start_time
+        self.running = False
 
     def elapsed_time(self):
-        pass
+        if self.running:
+            return datetime.datetime.now() - self.start_time
+        else:
+            return self.end_time - self.start_time
